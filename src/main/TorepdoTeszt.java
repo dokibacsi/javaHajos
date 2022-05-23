0442ed65f68441a67d8dc97934a83e5553621529
sw4q3ar2 <biggercsgo@gmail.com>
2022-05-23 12:19:00 +0200

    TorpedoTeszt CLASS

---
+++ src/main/TorepdoTeszt.java
@@ -0,0 +1,10 @@
+package main;
+public class TorepdoTeszt {
+    public static void main(String[] args) {
+        System.out.println("Ez egy sikeres teszt!");
+    }
+    
+    public String tesztLoves(int poz){
+        return "";
+    }
+}
